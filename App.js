ea8c01c40008e653ba26981ee416c14b8be256e0
sr33ni <77060235+sr33ni@users.noreply.github.com>
2024-07-28 21:16:30 +0530

    new file from upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import ClickableComponent from './New folder/clickableComponent';
+import Anchor_page from './react_app/anchor_page';
+
+function App() {
+  return (
+    <div className="App">
+          
+      <Anchor_page />
+
+    </div>
+  );
+}
+
+export default App;
