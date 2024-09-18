2cf21dd06e3c492e3cf7729b9050a822bae1fc84
sreeni <sreenivas.s@zerone-consulting.com>
2024-09-18 16:20:53 +0530

    react_app | json c01

--- src/react_app/sample_page.js
+++ src/react_app/sample_page.js
@@ -1,27 +1,28 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 
 const Sample_page = () => {
      const [color, setColor] = useState("null")
 
-     const [blogs, setBlogs] = useState([
-        {id :  1,
-        blogname : "red paint",
-        blogcontent : "Lorem ipsum dolor sit amet, eam ne suavitate iudicabit patrioque. Nec agam alterum necessitatibus ne, sit eu tibique iracundia reformidans, ius tritani denique ex. Tantas oblique nominati et eum, impetus meliore consectetuer eam ne. No per quando lobortis, eam ea liber gubergren disputationi, ut quo brute salutandi.Mel ullum nulla an, tempor tacimates te mea. Vel ornatus disputando ea, cu sea dolore noster adipiscing. His ex vide ignota convenire. Cu vim alia voluptatum, nam et labore persecuti. Vis diam rebum atqui ex, mel iuvaret deterruisset in. Wisi veritus feugait ne vix, ex explicari adipiscing mea. Ne eleifend aliquando eos, sed dolore fabellas "
-        },
-        {id :  2,
-            blogname : "Startdust",
-            blogcontent : "Lorem ipsum dolor sit amet, eam ne suavitate iudicabit patrioque. Nec agam alterum necessitatibus ne, sit eu tibique iracundia reformidans, ius tritani denique ex. Tantas oblique nominati et eum, impetus meliore consectetuer eam ne. No per quando lobortis, eam ea liber gubergren disputationi, ut quo brute salutandi.Mel ullum nulla an, tempor tacimates te mea. Vel ornatus disputando ea, cu sea dolore noster adipiscing. His ex vide ignota convenire. Cu vim alia voluptatum, nam et labore persecuti. Vis diam rebum atqui ex, mel iuvaret deterruisset in. Wisi veritus feugait ne vix, ex explicari adipiscing mea. Ne eleifend aliquando eos, sed dolore fabellas "
-            },
-            {id :  3,
-                blogname : "killer croc",
-                blogcontent : "Lorem ipsum dolor sit amet, eam ne suavitate iudicabit patrioque. Nec agam alterum necessitatibus ne, sit eu tibique iracundia reformidans, ius tritani denique ex. Tantas oblique nominati et eum, impetus meliore consectetuer eam ne. No per quando lobortis, eam ea liber gubergren disputationi, ut quo brute salutandi.Mel ullum nulla an, tempor tacimates te mea. Vel ornatus disputando ea, cu sea dolore noster adipiscing. His ex vide ignota convenire. Cu vim alia voluptatum, nam et labore persecuti. Vis diam rebum atqui ex, mel iuvaret deterruisset in. Wisi veritus feugait ne vix, ex explicari adipiscing mea. Ne eleifend aliquando eos, sed dolore fabellas "
-                },        {id :  4,
-                    blogname : "magic men",
-                    blogcontent : "Lorem ipsum dolor sit amet, eam ne suavitate iudicabit patrioque. Nec agam alterum necessitatibus ne, sit eu tibique iracundia reformidans, ius tritani denique ex. Tantas oblique nominati et eum, impetus meliore consectetuer eam ne. No per quando lobortis, eam ea liber gubergren disputationi, ut quo brute salutandi.Mel ullum nulla an, tempor tacimates te mea. Vel ornatus disputando ea, cu sea dolore noster adipiscing. His ex vide ignota convenire. Cu vim alia voluptatum, nam et labore persecuti. Vis diam rebum atqui ex, mel iuvaret deterruisset in. Wisi veritus feugait ne vix, ex explicari adipiscing mea. Ne eleifend aliquando eos, sed dolore fabellas "
-                    }
-     ]);
+     const [blogs, setBlogs] = useState(null);
      const onclickhandle = () => {
          setColor(!color)
+    }
+    const [filter, setfilter] = useState("null")
+    useEffect(
+        () => {
+            fetch('http://localhost:8000/blogs-data')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log("kiki");
+                setBlogs(data)
+            })
+        },[]
+    )
+    let blogdelete = (id) =>{
+        const newBlog = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlog)
     }
     return(
         <div className="login-container">
@@ -29,13 +30,19 @@ const Sample_page = () => {
             {/* <button onClick={() => {onclickhandle()}} className={(color ? "jkl" : "red")}>sss</button> */}
             <h2> hello {color ? "red" : "noon"}man</h2>
             <button onClick={onclickhandle} className={(color ? "jkl" : "red")}>sss</button>
+            <select>
+                    <option>aut</option>
+                    <option>new</option>
+                </select>
             <div className="blog-container">
-                {
+                {blogs &&
                     blogs.map((blog) => 
                         (
                             <div className="blog-card" key={blog.id}>
                                 <h1 className="red">{blog.blogname}</h1>
                                 <p>{blog.blogcontent}</p>
+                                <h3>{blog.blogauthor}</h3>
+                                <button onClick= {blogdelete}>delete</button>
                             </div>
                         ))
                 }
