a64d8f78a112fece0087bfd0790852322b1396e8
sreeni <sreenivas.s@zerone-consulting.com>
2024-09-30 18:22:20 +0530

    new change c01
    npx json-server --watch sample_page.json --port 8000

--- src/react_app/sample_page.js
+++ src/react_app/sample_page.js
@@ -1,13 +1,31 @@
 import React, { useEffect, useState } from "react"
+import Blog_component from "./blog_component"
 
 const Sample_page = () => {
      const [color, setColor] = useState("null")
 
      const [blogs, setBlogs] = useState(null);
      const onclickhandle = () => {
+        alert("red");
          setColor(!color)
     }
-    const [filter, setfilter] = useState("null")
+//     const bands = [
+//         {
+//     "id" :  1,
+//     "bandname" : "led zeppelin",
+//     "genre": "rock",
+//     "about" : "Lorem ipsum dolor sit amet, eam ne suavitate iudicabit patrioque. Nec agam alterum necessitatibus ne, sit eu tibique iracundia reformidans, ius tritani denique ex. Tantas oblique nominati et eum, impetus meliore consectetuer eam ne. No per quando "
+// },
+// {
+//     "id":  2,
+//     "bandname" : "Nirvana",
+//     "genre": "grung",
+//     "about" : "Lorem ipsum dolor sit amet, eam ne suavitate iudicabit patrioque. Nec agam alterum necessitatibus ne, sit eu tibique iracundia reformidans, ius tritani denique ex. Tantas oblique nominati et eum, impetus meliore consectetuer eam ne. No per quando "
+
+// }
+        
+//          ]
+//      const [filter, setfilter] = useState("null")
     useEffect(
         () => {
             fetch('http://localhost:8000/blogs-data')
@@ -15,40 +33,49 @@ const Sample_page = () => {
                 return res.json();
             })
             .then(data => {
-                console.log("kiki");
                 setBlogs(data)
+                console.log("koo");
             })
         },[]
     )
     let blogdelete = (id) =>{
         const newBlog = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlog)
+        setBlogs(newBlog);
     }
     return(
-        <div className="login-container">
+        <div>
+                <div lassName="login-container">
             <h1> hello {color ? "red" : "noon"}man</h1>
-            {/* <button onClick={() => {onclickhandle()}} className={(color ? "jkl" : "red")}>sss</button> */}
+            <h1> hello {color ? "red" : "noon"}man</h1>
+            <button onClick={() => {onclickhandle()}} className={(color ? "jkl" : "red")}>sss</button>
             <h2> hello {color ? "red" : "noon"}man</h2>
-            <button onClick={onclickhandle} className={(color ? "jkl" : "red")}>sss</button>
+            <button onClick={() => {onclickhandle()}} className={(color ? "jkl" : "red")}>sss</button>
             <select>
                     <option>aut</option>
                     <option>new</option>
                 </select>
-            <div className="blog-container">
-                {blogs &&
-                    blogs.map((blog) => 
-                        (
-                            <div className="blog-card" key={blog.id}>
-                                <h1 className="red">{blog.blogname}</h1>
-                                <p>{blog.blogcontent}</p>
-                                <h3>{blog.blogauthor}</h3>
-                                <button onClick= {blogdelete}>delete</button>
-                            </div>
+
+                {/* <div className="band-container">
+                    {
+                        bands.map((band) =>(
+                                <div key={band.id}>
+                                    <h1>{band.bandname}</h1>
+                                    <h2>{band.genre}</h2>
+                                    <h3>{band.about}</h3>
+                                </div>
+
                         ))
-                }
-            </div>
+                    }
+
+                </div> */}
+
 
         </div>
+        { blogs && <Blog_component  blogs={blogs} blog-delete={blogdelete} title="all Blogs"/>}
+        { blogs &&           <Blog_component blogs={blogs.filter(blog => blog.blogauthor == "Spencer" )} blog-delete={blogdelete} title="spencer Blogs"/>}
+
+        </div>
+
     )
 }
 
