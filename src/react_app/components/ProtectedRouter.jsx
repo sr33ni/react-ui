46c20fb1e17e09fafbc6268be3582a26c5f5cd50
sreeni <sreenivas.s@zerone-consulting.com>
2025-10-22 12:33:15 +0530

    new commit login new

--- src/react_app/components/ProtectedRouter.jsx
+++ src/react_app/components/ProtectedRouter.jsx
@@ -1,8 +1,8 @@
-import { Navigate, Outlet } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = () => {
   const user = localStorage.getItem("user");
-  return user ? <Outlet /> : <Navigate to="/login" replace />;
+  return user ? <Navigate to="/home" replace /> : <Navigate to="/login" replace />;
 };
 
 export default ProtectedRoute;
