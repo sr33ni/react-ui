362c9d5493868879536fd86dc46ca36d99f34907
sreeni <sreenivas.s@zerone-consulting.com>
2025-11-11 18:49:59 +0530

    new changes

--- src/react_app/sub_page/home_page.js
+++ src/react_app/sub_page/home_page.js
@@ -1,17 +1,35 @@
 
-import React, {useState , Component} from 'react';
-class Home_page extends React.Component{
-  constructor(props) {
-    super(props);
+import React, {useState , Component, useEffect} from 'react';
+const Home_page = ()=>{
+
+  const [sample, setSample] = useState("testing");
+  const [active, setActive] = useState("active");
+
+  //   useEffect(
+  //   ()=> {handleClick()},["testing"]
+  // )
+
+  const handleClick = ()=> {
+    if(sample == "testing"){
+      setSample("working")
+      setActive("active")
+    }else{
+      setSample("testing")
+      setActive("")
+    }
   }
 
-  render(){
+
     return(
-        <div className='home_detail_tab'>
+              <div className='home_detail_tab'>
             <h1>Home</h1>
             <p>Welcome to home page.</p>
+            <p className={active}>{sample}</p>
+
+            <button onClick={()=> {handleClick()}}>
+              Click me
+            </button>
         </div>
-    );
+    )
   }
-}
 export default Home_page;
